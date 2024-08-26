e9d3e3ce55f087177920a7f10b73faba2bffde0c
Julio C. Estrada <estradacjulio@gmail.com>
2024-08-26 17:52:53 -0400

    feat: align Asciidoctor Layout and Styling to Antora
    * media queries selector flow
    * update layout and improved responsiveness
    * class selector and nav font

--- boostlook.rb
+++ boostlook.rb
@@ -1,7 +1,7 @@
 Asciidoctor::Extensions.register do
   postprocessor do
     process do |doc, output|
-      output = output.sub(/<body (.*?)>/, '<body \\1><div class="doc">')
+      output = output.sub(/<body (.*?)>/, '<body \\1><div class="boostlook">')
       output = output.sub(/<\/body>/, "</div></body>")
       #output = output.sub(/<\/body>/, "</body>")
     end
